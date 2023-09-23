5e318822bec6e89746c71667f972d610132400d5
HP Nguyen <phucvovinam97@gmail.com>
2023-09-23 14:34:38 +0700

    Show MovieDetail on Search page

--- src/components/Search/ResultList.js
+++ src/components/Search/ResultList.js
@@ -1,10 +1,19 @@
-import { useEffect, useState } from 'react';
-import './ResultList.css';
+import movieTrailer from 'movie-trailer';
+import { useEffect, useRef, useState } from 'react';
 import { fetchData, requests } from '../../api';
+import MovieDetail from '../Browse/MovieDetail';
 import MovieItem from '../Browse/MovieItem';
+import './ResultList.css';
 
 export default function ResultList({ query }) {
+  // State lưu trữ danh sách các bộ phim
   const [movies, setMovies] = useState([]);
+  // State lưu trữ phim được chọn
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  // State lưu trữ key của trailer hoặc teaser của phim được chọn
+  const [trailerKey, setTrailerKey] = useState('');
+  // State lưu trữ backdrop của phim được chọn
+  const [backdrop, setBackdrop] = useState('');
   useEffect(() => {
     // Kiểm tra có từ khóa hay không
     if (!query) return setMovies([]);
@@ -12,14 +21,59 @@ export default function ResultList({ query }) {
     fetchData(requests.fetchSearch(query))
       .then(respone => setMovies(respone.data.results))
       .catch(err => console.error('Error fetching search results:', err));
+    // Reset selected movie khi search phim mới
+    setSelectedMovie(null);
   }, [query]);
 
+  // Xử lý sự kiện khi click vào một phim
+  const handleClick = movie => {
+    // Đóng phần detail khi click thêm lần nữa vào phim đang chọn
+    if (selectedMovie && selectedMovie.id === movie.id) {
+      setSelectedMovie(null);
+    } else {
+      // Hiển thị phần detail khi chọn 1 phim
+      setSelectedMovie(movie);
+      // Lấy key của trailer hoặc teaser
+      movieTrailer(movie?.title || '')
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerKey(urlParams.get('v'));
+        })
+        .catch(error => {
+          // Xử lý lỗi khi không lấy được key của trailer
+          console.error(error);
+          setTrailerKey('');
+          // Lấy backdrop thay thế cho trailer
+          setBackdrop(movie['backdrop_path']);
+        });
+    }
+  };
+  // Scroll tới MovieDetail khi chọn 1 phim
+  const detailPosition = useRef(null);
+  useEffect(() => {
+    detailPosition.current.scrollIntoView({ behavior: 'smooth' });
+  }, [selectedMovie]);
   return (
     <div className="result-list">
-      <h2 className="result-list-title">Search Results</h2>
+      <h2 ref={detailPosition} className="result-list-title">
+        Search Results
+      </h2>
+      {selectedMovie && (
+        <MovieDetail
+          movieData={selectedMovie}
+          movieTrailer={trailerKey}
+          backdropImage={backdrop}
+        />
+      )}
       <ul className="movies-list">
         {movies.map(movie => (
-          <MovieItem key={movie.id} movie={movie} isVertical={true} />
+          <li key={movie.id}>
+            <MovieItem
+              movie={movie}
+              isVertical={true}
+              onClick={() => handleClick(movie)}
+            />
+          </li>
         ))}
       </ul>
     </div>
